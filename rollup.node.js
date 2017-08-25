bb4e6713808e9d5d28f2bc5821fcf3a131fd3d64
Frank Fox <ffox77@yahoo.com>
2017-08-25 08:52:48 -0700

    add package-lock.json so all devs will get the same npm module versions

--- rollup.node.js
+++ rollup.node.js
@@ -1,12 +1,12 @@
-let fs = require('fs');
-let rollup = require('rollup');
-let dependencies = require('./package.json').dependencies;
+var fs = require('fs');
+var rollup = require('rollup');
+var dependencies = require('./package.json').dependencies;
 
 rollup.rollup({
   entry: './src/neo/index.js',
   external: Object.keys(dependencies)
 }).then(function(bundle) {
-  let code = bundle.generate({format: 'cjs'}).code;
+  var code = bundle.generate({format: 'cjs'}).code;
   return new Promise(function(resolve, reject) {
     fs.writeFile('dist/neo.node.js', code, 'utf8', function(error) {
       if (error) return reject(error);
@@ -19,7 +19,7 @@ rollup.rollup({
     entry: './src/neo/core.index.js',
     external: Object.keys(dependencies)
 }).then(function(bundle) {
-    let code = bundle.generate({format: 'cjs'}).code;
+    var code = bundle.generate({format: 'cjs'}).code;
     return new Promise(function(resolve, reject) {
         fs.writeFile('dist/neo.node.core.js', code, 'utf8', function(error) {
             if (error) return reject(error);

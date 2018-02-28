2596907fe37edc4446d267882289ecf13863500e
Frank Fox <ffox77@yahoo.com>
2018-02-28 10:34:39 -0800

    added latency option to REST calls

--- src/neo/services/serviceOptions.js
+++ src/neo/services/serviceOptions.js
@@ -3,19 +3,35 @@ import { getProtocolClient } from '../registry.js';
 export function serviceOptions(service, serviceName, initObj) {
 
     if (typeof initObj === 'string') {
-        initObj = { baseUrl: initObj};
+        initObj = { baseUrl: initObj };
     }
     else if (typeof initObj !== 'object') {
         initObj = {};
     }
 
+    service.serviceLatency = 0;
+    service.serviceLatencyStartTime = 0;
     service.serviceName = serviceName;
     service.serviceBaseUrl = initObj.baseUrl || '';
     service.servicePollInterval = initObj.poll;
+    service.serviceMonitorLatency = initObj.monitorLatency;
 
     service.baseUrl = baseUrl;
     service.protocolClient = protocolClient;
     service.poll = poll;
+    service.monitorLatency = monitorLatency;
+    service.startLatencyTimer = startLatencyTimer;
+    service.stopLatencyTimer = stopLatencyTimer;
+    service.latency = latency;
+
+
+    function startLatencyTimer () {
+        service.serviceLatencyStartTime = Date.now();
+    }
+
+    function stopLatencyTimer () {
+        service.serviceLatency = Date.now() - service.serviceLatencyStartTime;
+    }
 
     function baseUrl (val) {
 
@@ -46,6 +62,29 @@ export function serviceOptions(service, serviceName, initObj) {
         }
 
         this.servicePollInterval = val;
+
+        return this;
+    }
+
+    function monitorLatency (val) {
+
+        if (!val) {
+            return this.serviceMonitorLatency;
+        }
+
+        this.serviceMonitorLatency = val;
+
+        return this;
+    }
+
+    function latency (val) {
+
+        if (!val) {
+            return this.serviceLatency;
+        }
+
+        //read-only
+        //this.serviceLatency = val;
 
         return this;
     }
